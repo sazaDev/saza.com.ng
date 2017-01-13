c012a1d4bdc536ff713052ab9f5419fd8f848b5e
Peter Oliha <poliha2002@yahoo.com>
2017-01-13 19:09:43 +0100

    manage data view and controller

---
+++ lumens/app/account/manage_data.controller.js
@@ -0,0 +1,56 @@
+var lumensWall = angular.module('lumensWall');
+
+lumensWall.controller('manageDataController', function($scope, $state, $http, $rootScope, Account, User) {
+  
+  $scope.manageData = {};
+  $scope.statusMsg = false;
+  
+  
+
+  $scope.init = function() {
+    $scope.manageData.id = $rootScope.currentUser.id;
+    $scope.manageData.email = $rootScope.currentUser.email;
+    $scope.manageData.token = $rootScope.currentUser.token;
+    
+  };
+  
+  $scope.closeAlert = function() {
+    $scope.statusMsg = {};
+  };
+
+  $scope.manageData = function() {
+    $scope.manageData.id = $rootScope.currentUser.id;
+    $scope.manageData.email = $rootScope.currentUser.email;
+    $scope.manageData.token = $rootScope.currentUser.token;
+    $scope.manageData.account_id = $rootScope.currentUser.currentAccount;
+    
+    
+    Account.manageData($scope.manageData)
+      .success(function(data) {
+
+        // console.log("success",data);
+        // show success message
+        $scope.statusMsg = {};
+        $scope.statusMsg.type = 'alert-success';
+        $scope.statusMsg.content = data.content.message;
+        $scope.manageData = {};
+        window.scrollTo(0, 0);
+
+      })
+      .error(function(data) {
+        
+        $scope.statusMsg = {};
+        $scope.statusMsg.type = 'alert-danger';
+        $scope.statusMsg.content = data.content.message;
+        // $scope.manageData = {};
+        window.scrollTo(0, 0);
+
+        
+      });
+  };
+
+
+
+
+
+});
