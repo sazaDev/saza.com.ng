a4ce1fed8c9b9239d0fa81240748c7c5decd0e43
Peter Oliha <poliha2002@yahoo.com>
2017-01-20 22:27:03 +0100

    menu fix and widow.postmessage fix for social login

--- lumens/app/login/login.controller.js
+++ lumens/app/login/login.controller.js
@@ -65,33 +65,64 @@ lumensWall.controller('loginController', function($scope, $state, $window, $inte
     var popup = $window.open(url, type+' Login', 'width=' + width + ',height=' + height + ',scrollbars = 0, top=' + top + ',left=' + left);
 
 
- 		var i = $interval(function(){
-      interval += 500;
-      try {
+ 		// var i = $interval(function(){
+   //    interval += 500;
+   //    try {
 
-       if (popup.value){
-       		console.log("popup value", popup.value);
-       		popup.value.authenticated = true;
-					// set the currently active account
+   //     if (popup.value){
+   //     		console.log("popup value", popup.value);
+   //     		popup.value.authenticated = true;
+			// 		// set the currently active account
 				
-					if (popup.value.accounts.length > 0) {
-						popup.value.currentAccount = popup.value.accounts[0].account_id;
-						popup.value.currentUsername = popup.value.accounts[0].fed_name;
+			// 		if (popup.value.accounts.length > 0) {
+			// 			popup.value.currentAccount = popup.value.accounts[0].account_id;
+			// 			popup.value.currentUsername = popup.value.accounts[0].fed_name;
+			// 		}
+
+   //     		User.set(popup.value);
+   //     		var user = JSON.stringify(popup.value);
+				
+			// 		// Set the stringified user data into local storage
+   //      	localStorage.setItem('user', user);
+   //        $interval.cancel(i);
+   //        popup.close();
+   //        $state.go('dashboard');
+   //      }
+   //    } catch(e){
+   //      console.error(e);
+   //    }
+   //  }, interval);        
+	
+		window.addEventListener('message', function(event) {
+
+    var origin = event.origin || event.originalEvent.origin;
+      // IMPORTANT: Check the origin of the data! 
+    if (origin === 'https://saza.com.ng:8888') {
+          // The data has been sent from your site 
+
+          // The data sent with postMessage is stored in event.data 
+          console.log(event.data);
+          var user = event.data;
+          if (user.accounts.length > 0) {
+						user.currentAccount = user.accounts[0].account_id;
+						user.currentUsername = user.accounts[0].fed_name;
 					}
+					User.set(user);
+					localStorage.setItem('user', JSON.stringify(user));
+					popup.close();
+					$state.go('dashboard');
 
-       		User.set(popup.value);
-       		var user = JSON.stringify(popup.value);
-				
-					// Set the stringified user data into local storage
-        	localStorage.setItem('user', user);
-          $interval.cancel(i);
+      } else {
+          // The data hasn't been sent from your site! 
+          // Be careful! Do not use it.
+          console.log(event);
+          console.log("not from site2");
           popup.close();
-          $state.go('dashboard');
-        }
-      } catch(e){
-        console.error(e);
+          alert("Invalid response. User not authenticated");
+          return;
       }
-    }, interval);        
+  });
+
 		
 
 	};
