35c7448e0c047527dd0649bd4b3319d5c955ba8f
Peter Oliha <poliha2002@yahoo.com>
2017-06-12 20:41:34 +0100

    save tx passphrase

--- lumens/app/account/save_passphrase.controller.js
+++ lumens/app/account/save_passphrase.controller.js
@@ -3,10 +3,10 @@ var lumensWall = angular.module('lumensWall');
 lumensWall.controller('savePassphraseController', function($scope, $state, $http, $rootScope, Account, User) {
 
 console.log($rootScope.currentUser);
- if ($rootScope.currentUser.need_password == 0) {
-          // event.preventDefault();
-              $state.go('dashboard');
-   }
+ // if ($rootScope.currentUser.need_password == 0) {
+ //          // event.preventDefault();
+ //              $state.go('dashboard');
+ //   }
 
   $scope.userData = {};
   $scope.linkData = {};
@@ -24,44 +24,24 @@ console.log($rootScope.currentUser);
     Account.savePassphrase($scope.userData)
     .success(function(data) {
 
-      //console.log(da
       data.content.data.authenticated = true;
-      
-      // store seed to display in browser
-      // $scope.userSeed = data.content.data.seed;
-      // $scope.userAcct = data.content.data.account_id;
-      
+
       // set the currently active account
       if (data.content.data.accounts.length > 0) {
         data.content.data.currentAccount = data.content.data.accounts[0].account_id;
         data.content.data.currentUsername = data.content.data.accounts[0].fed_name;
       }
 
-
       var user = data.content.data;
-      user.seed = "";
-      user.account_id = "";
-      // localUser = JSON.stringify(user);
-      
+
+      User.set(user);
 
       // Set the stringified user data into local storage
-      // localStorage.setItem('user', localUser);
-
-      // $rootScope.authenticated = true;
-
-      // $rootScope.currentUser = user;
-      // console.log("currentUser", $rootScope.currentUser);
-        User.set(user);
-
-        
-        // Set the stringified user data into local storage
-        localStorage.setItem('user', JSON.stringify(user));
-        console.log("currentUser", User.get());
-        $scope.statusMsg = {};
-        $scope.statusMsg.type = 'alert-success';
-        $scope.statusMsg.content = data.content.message;
-      // angular.element('.mb-control-success').triggerHandler('click');
-                
+      localStorage.setItem('user', JSON.stringify(user));
+      console.log("currentUser", User.get());
+      $scope.statusMsg = {};
+      $scope.statusMsg.type = 'alert-success';
+      $scope.statusMsg.content = data.content.message;
 
     })
     .error(function(data) {
@@ -69,7 +49,7 @@ console.log($rootScope.currentUser);
       $scope.statusMsg = {};
       $scope.statusMsg.type = 'alert-danger';
       $scope.statusMsg.content = data.content.message;
-        
+
     });
   };
 
