671066feb83c69271c018dfaf96191f8709f9472
Peter Oliha <poliha2002@yahoo.com>
2017-06-15 12:49:39 +0100

    added a config file

---
+++ lumens/app/shared/services/config.js
@@ -0,0 +1,25 @@
+
+(function() {
+  var Config = {
+
+"Stellar" : {
+    "testNetwork": "https://horizon-testnet.stellar.org",
+    "liveNetwork": "https://horizon.stellar.org",
+    "homeDomain": "saza.com.ng",
+    "homeAccount": "GDWTOOMU7BTHAYGKKTCLKXHEF56DHUXHYISSAPNJ2JDRYPYHUFY3HIXH",
+    "inflationDest": "GDWTOOMU7BTHAYGKKTCLKXHEF56DHUXHYISSAPNJ2JDRYPYHUFY3HIXH",
+    "testHomeDomain": "saza.com.ng",
+    "testHomeAccount": "GD3LJIQEWEOWKEKW7N7YX255RFJKWLVKIU457LUZPWP22I3HLR5WOCBK",
+    "testInflationDest": "GD3LJIQEWEOWKEKW7N7YX255RFJKWLVKIU457LUZPWP22I3HLR5WOCBK",
+    "anchorLimit": "200"
+  },
+  "General": {
+    "production": false,
+  },
+
+
+};
+ window.Config = Config;
+})();
+
+
