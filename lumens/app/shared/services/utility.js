6010a58cd2775ac47a600788d7bc26528b67f729
Peter Oliha <poliha2002@yahoo.com>
2017-06-15 20:42:12 +0100

    added isEmpty()

--- lumens/app/shared/services/utility.js
+++ lumens/app/shared/services/utility.js
@@ -174,6 +174,13 @@ var StellarSDK = StellarSdk;
 
     }
   },
+  isEmpty: function(obj) {
+    for(var key in obj) {
+        if(obj.hasOwnProperty(key))
+            return false;
+    }
+    return true;
+  },
 
 
 };
