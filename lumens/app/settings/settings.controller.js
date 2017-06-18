10eff1107a6eb7ead3319f74e72539761ac3287e
Peter Oliha <poliha2002@yahoo.com>
2017-06-19 00:59:25 +0100

    enable, activate and disable 2fa

--- lumens/app/settings/settings.controller.js
+++ lumens/app/settings/settings.controller.js
@@ -1,14 +1,20 @@
 var lumensWall = angular.module('lumensWall');
+var QRCode = qrcodelib;
 
 lumensWall.controller('settingsController', function($scope, $state, $http, $rootScope, Account, User) {
 
 	$scope.passwordData = {};
   $scope.userData = {};
+  $scope.tfaData = {};
   $scope.deleteData = {};
 	$scope.statusMsg = null;
 	$scope.seed = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
 	$scope.tempSeed = "";
 	$scope.showSeed = false;
+  $scope.show_qr = false;
+  $scope.otpUrl = "../img/logo.png";
+  $scope.otpSecretKey = "";
+  $scope.otp_id = "";
 	$scope.init = function() {
 		// load the seed here but dont display yet
     // Account.getSeed($rootScope.currentUser.token, $rootScope.currentUser.currentAccount)
@@ -176,13 +182,23 @@ lumensWall.controller('settingsController', function($scope, $state, $http, $roo
 
 	};
 
-  $scope.enable2fa = function() {
+  $scope.get2faSecret = function() {
     $scope.userData.token = $rootScope.currentUser.token;
     $scope.userData.id = $rootScope.currentUser.id;
 
-    Account.enable2fa($scope.userData)
+    Account.get2faSecret($scope.userData)
     .then(function(resp) {
       console.log(resp);
+      $scope.show_qr = true;
+      var respContent = resp.data.content;
+      $scope.otpSecretKey = respContent.data.auth_code;
+      $scope.otp_id = respContent.data.otp_id;
+      QRCode.toDataURL(respContent.data.image_url, function (err, url) {
+        console.log(err);
+        console.log(url);
+        $scope.otpUrl = url;
+
+      });
 
     })
     .catch(function(data) {
@@ -193,5 +209,110 @@ lumensWall.controller('settingsController', function($scope, $state, $http, $roo
 
 
   };
+
+  $scope.enable2fa = function(secret) {
+    $scope.userData.token = $rootScope.currentUser.token;
+    $scope.userData.id = $rootScope.currentUser.id;
+    $scope.userData.otpSecretKey = secret;
+    $scope.userData.otp_id = $scope.otp_id;
+    window.scrollTo(0, 0);
+    if ($scope.userData.otpToken) {
+      Account.enable2fa($scope.userData)
+        .then(function(resp) {
+          console.log(resp);
+          var respContent = resp.data.content;
+          respContent.data.authenticated = true;
+          // set the currently active account
+          if (respContent.data.accounts.length > 0) {
+            respContent.data.currentAccount = respContent.data.accounts[0].account_id;
+            respContent.data.currentUsername = respContent.data.accounts[0].fed_name;
+          }
+
+          var user = respContent.data;
+
+          User.set(user);
+
+          // Set the stringified user data into local storage
+          localStorage.setItem('user', JSON.stringify(user));
+          console.log("currentUser", User.get());
+          $scope.statusMsg = {};
+          $scope.statusMsg.type = 'alert-success';
+          $scope.statusMsg.content = respContent.message;
+          // $scope.show_qr = false;
+
+        })
+        .catch(function(data) {
+          console.log(data);
+          $scope.statusMsg = {};
+          $scope.statusMsg.type = 'alert-danger';
+          if (data.content) {
+            $scope.statusMsg.content = data.content.message;
+          } else{
+            $scope.statusMsg.content = data.data.content.message;
+          }
+
+
+        });
+
+
+    } else{
+
+      $scope.statusMsg = {};
+      $scope.statusMsg.type = 'alert-danger';
+      $scope.statusMsg.content = ['Invalid Code'];
+      // $scope.$apply();
+
+    }
+
+  };
+
+
+  $scope.disable2fa = function() {
+    $scope.tfaData.token = $rootScope.currentUser.token;
+    $scope.tfaData.id = $rootScope.currentUser.id;
+
+    
+    window.scrollTo(0, 0);
+
+      Account.disable2fa($scope.tfaData)
+        .then(function(resp) {
+          console.log(resp);
+          var respContent = resp.data.content;
+          respContent.data.authenticated = true;
+          // set the currently active account
+          if (respContent.data.accounts.length > 0) {
+            respContent.data.currentAccount = respContent.data.accounts[0].account_id;
+            respContent.data.currentUsername = respContent.data.accounts[0].fed_name;
+          }
+
+          var user = respContent.data;
+
+          User.set(user);
+
+          // Set the stringified user data into local storage
+          localStorage.setItem('user', JSON.stringify(user));
+          console.log("currentUser", User.get());
+          $scope.statusMsg = {};
+          $scope.statusMsg.type = 'alert-success';
+          $scope.statusMsg.content = respContent.message;
+          // $scope.show_qr = false;
+          $state.go('settings', {}, {reload: true});
+
+        })
+        .catch(function(data) {
+          console.log(data);
+          $scope.statusMsg = {};
+          $scope.statusMsg.type = 'alert-danger';
+          if (data.content) {
+            $scope.statusMsg.content = data.content.message;
+          } else{
+            $scope.statusMsg.content = data.data.content.message;
+          }
+
+
+        });
+
+  };
+
 
 });
